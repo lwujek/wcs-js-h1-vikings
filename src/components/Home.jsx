2fcdf46a86a7c3ea7644f7d9f57ae68aff0aa7e3
Laura <laura.wujek@gmail.com>
2020-11-19 18:50:44 +0100

    correction navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './Header.jsx';
 import './Home.css';
 import Filter from './Filter.jsx';
+import Navbar from './Navbar.jsx';
 
 
 const Home = () => {
     return(
         <div className='home'>
+            <Navbar />
             <Header />
             <Filter />
         </div>
