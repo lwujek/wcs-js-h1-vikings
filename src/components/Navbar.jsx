9246b88bcc015e4ef50570eeb8a1c4c3165f2906
baptiste_gfy <geffroy.baptiste@outlook.fr>
2020-11-19 16:29:09 +0100

    navbar create

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { GiVikingHelmet, GiVikingChurch } from "react-icons/gi";
+import { IconContext } from "react-icons";
+import { Link } from "react-router-dom";
+
+const Section = styled.section`
+  width: 100vw;
+  height: 10vh;
+  background: #c4c4c4;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .btn {
+    background-image: 
+    width: 6rem;
+    height: 90%;
+    margin: 0 1rem;
+  }
+`;
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <IconContext.Provider value={{ size: "6rem", color: "#000" }}>
+        <Section>
+          <Link to="/" 
+          <GiVikingChurch className="btn" />
+          <GiVikingHelmet className="btn" />
+        </Section>
+      </IconContext.Provider>
+    );
+  }
+}
